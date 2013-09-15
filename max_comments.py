7df5c8e9ca9b47d069c9645556d579504d7e9de7
ap-Codkelden <mavladi@gmail.com>
2013-09-16 01:31:43 +0300

    Calc max comments count

---
+++ max_comments.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+"""
+Calculation max number of the comments
+"""
+
+import json
+import sys
+from  user_ops import db_handle
+
+def max_comments():
+	db = db_handle()
+	
+	db.create_user_list()
+	db.cursor.execute("SELECT u.uname, COUNT(c.mid)  FROM users u \
+		LEFT OUTER JOIN comments c \
+		ON u.uid = c.uid \
+		GROUP BY u.uid, u.uname \
+		ORDER BY 2 DESC")
+	mc = db.cursor.fetchall()
+	db.delete_user_list()
+
+	with open("max_comments.json", 'w') as mc_file:
+		json.dump(mc, mc_file,  ensure_ascii=False)
+	sys.stdout.write('Data saved in max_comments.json\n')
+
+
+if __name__ == "__main__":
+	max_comments()
