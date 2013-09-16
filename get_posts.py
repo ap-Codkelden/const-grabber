81836b23da42e441c43232bc3b338e9e2360762a
ap-Codkelden <mavladi@gmail.com>
2013-09-16 15:49:13 +0300

    field msgid => mid

--- get_posts.py
+++ get_posts.py
@@ -13,7 +13,11 @@ arg_parser = argparse.ArgumentParser()
 arg_parser.add_argument('-v', '--verbose', help='print messages', \
                         action='store_true', default=False, dest='verbose')
 
+arg_parser.add_argument('-l', '--logging', help='write errors log', \
+                        action='store_true', default=False, dest='log')
+
 URL_STRING = """http://api.juick.com/messages?tag=%D1%81%D0%BE%D0%B7%D0%B2%D0%B5%D0%B7%D0%B4%D0%B8%D0%B5&page="""
+URL_COMMENT = """http://api.juick.com/thread?mid="""
 
 def create_db():
 	global cursor
@@ -21,7 +25,10 @@ def create_db():
 	post_db = sqlite3.connect('post.db')
 	cursor = post_db.cursor()
 
-	cursor.execute('''CREATE TABLE if not exists posts (msgid integer, uid integer, uname text, body text, tags text, timestamp text, replies integer)''')
+	cursor.execute('''CREATE TABLE if not exists posts (mid integer, uid integer, uname text, body text, \
+		tags text, timestamp text, replies integer)''')
+	cursor.execute("""CREATE TABLE if not exists comments (mid integer, rid integer, replyto integer, \
+		uid integer, uname text, body text, timestamp text)""")
 	post_db.commit()
 
 
@@ -31,7 +38,7 @@ def GetContent(number, url=URL_STRING):
 		if args.verbose:
 			sys.stdout.write("Retrieve page %d...\n"  % number)
 		raw_content = text.read().replace(b"\t", b"")
-		readable = raw_content.decode('utf-8')
+		readable = raw_content.replace(b"'",b"''").decode('utf-8')
 		json_data = json.loads(readable)
 		return json_data
 	except urllib.error.HTTPError:
@@ -40,7 +47,7 @@ def GetContent(number, url=URL_STRING):
 
 def json_parser(posts):
 	for post in posts:
-		msgid = post['mid']
+		mid = post['mid']
 		uid = post['user']['uid']
 		uname = post['user']['uname']
 		body = post['body']
@@ -50,15 +57,16 @@ def json_parser(posts):
 			replies = post['replies']
 		except KeyError:
 			replies = 0
-		if msgid == last_post:
+		if mid == last_post:
 			if args.verbose:
 				sys.stdout.write("There are no new messages, stop fetching...\n\n")
 			return 0
 		else:
 			sql = """INSERT INTO posts VALUES (%d, %d, "%s", "%s", "%s", "%s", %d)""" % \
-			(msgid, uid, uname, body, tags, timestamp, replies)
+			(mid, uid, uname, body, tags, timestamp, replies)
 			cursor.execute(sql)
 			post_db.commit()
+			new_posts.append(mid)
 	return 1
 
 
@@ -68,17 +76,71 @@ def GetLastPost():
 	if result[0] == 0:
 		return 0
 	else:
-		cursor.execute("SELECT max(msgid) from posts")
+		cursor.execute("SELECT max(mid) from posts")
 		result=cursor.fetchone()
 		return result[0]
 
 
+def fetch_comment(comment):
+	try:
+		post = urllib.request.urlopen(URL_COMMENT + str(comment))
+		if args.verbose:
+			sys.stdout.write("Retrieve comments to #{0}...\n".format(comment))
+		post_raw_content = post.read().replace(b"\t", b"")
+		post_content = post_raw_content.decode('utf-8')
+		json_thread = json.loads(post_content)
+		if len(json_thread) > 1:
+			for c in range(0, len(json_thread)):
+				mid = json_thread[c]['mid']
+				if 'rid' in  json_thread[c]:
+					rid = json_thread[c]['rid']
+				else:
+					rid = 'null'
+				if 'replyto' in  json_thread[c]:
+					replyto = json_thread[c]['replyto']
+				else: 
+					replyto = 'null'
+				uid = json_thread[c]['user']['uid']
+				uname = json_thread[c]['user']['uname']
+				body = json_thread[c]['body'].replace("'","''")
+				timestamp = json_thread[c]['timestamp']
+				cursor.execute("INSERT INTO comments VALUES ({0},{1},{2},{3},'{4}','{5}','{6}')".format(mid, rid, replyto, uid, uname, body, timestamp))
+				post_db.commit()
+		else:
+			if args.verbose:
+				sys.stdout.write("Message #{0} have no replies, next...\n".format(comment))
+			return 0
+	except:
+		e = sys.exc_info()[0]
+		sys.stdout.write("Comments to message #{0} fetching error due to {1}.\n".format(comment, e))
+		if args.log:
+			logfile.write("\nErr_msg: {0}\n".format(e))
+			logfile.write("INSERT INTO comments VALUES ({0},{1},{2},{3},'{4}','{5}','{6}')".format(mid, rid, replyto, uid, uname, body, timestamp))
+		pass
+
+
 def get_comments():
-	pass
+	comm_dump = cursor.execute("SELECT DISTINCT mid from posts WHERE replies>0 and mid>{0}".format(last_post))
+	comments = comm_dump.fetchall()
+	for comment in comments:
+		fetch_comment(comment[0])
 
 
 def main():
+	"""
+	For production launch purposes, page_number variable always 
+	MUST be equal 1, but for debugging you can make it equal any 
+	non-negative integer lower than pages count 
+	"""
 	page_number = 1
+
+	"""
+	new_posts -- this is a list in which will store numbers 
+	of the new discovering messages 
+	"""
+	global new_posts
+	new_posts = []
+
 	try:
 		create_db()
 	except:
@@ -88,7 +150,7 @@ def main():
 	last_post = GetLastPost()
 	if args.verbose:
 		if last_post == 0:
-			sys.stdout.write("We are have no data about the number\nof the last post, is this 1st run?\n")
+			sys.stdout.write("We have no data about the number\nof the last post, is this 1st run?\n")
 		else:
 			sys.stdout.write("Last message is message #{0}\n".format(last_post))
 
@@ -102,6 +164,10 @@ def main():
 				break
 		else:
 			break
+
+	# We get the rockets, if its here :)
+	if len(new_posts) > 0:
+		get_comments()
 	
 	if args.verbose:
 		cursor.execute("SELECT COUNT(*) from posts")
@@ -112,4 +178,9 @@ def main():
 
 if __name__ == "__main__":
 	args = arg_parser.parse_args()
+	if args.log:
+		#global logfile
+		logfile = open('error.log', 'w')
 	main()
+	if args.log:
+		logfile.close()
